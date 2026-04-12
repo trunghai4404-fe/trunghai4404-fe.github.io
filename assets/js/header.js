b09219c0338d9a63bb7bcbec3c8c6fcb3e9932d7
TrungHai4404 <trunghai4404@gmail.com>
2026-04-12 21:45:19 +0700

    update: responsive mobile and add animation

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,3 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
         lastScroll = currentScroll;
     });
 });
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.site-header__nav-item a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.parentElement.classList.remove('active');
+
+        if (link.getAttribute('href') === `#${current}`) {
+            link.parentElement.classList.add('active');
+        }
+    });
+});
